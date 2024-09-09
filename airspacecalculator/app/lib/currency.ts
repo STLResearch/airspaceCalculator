6755ca2367f32e4f600a566ff8ca7a0777a30693
Victor Olowe <vikie95@gmail.com>
2024-09-09 14:46:33 +0100

    fix currency

--- airspacecalculator/app/lib/currency.ts
+++ airspacecalculator/app/lib/currency.ts
@@ -2,6 +2,7 @@ export function formatAmount(amount: number) {
   return new Intl.NumberFormat(undefined, {
     style: 'currency',
     currencyDisplay: 'narrowSymbol',
+    maximumFractionDigits: 0,
     currency: 'USD',
   }).format(amount);
 }
