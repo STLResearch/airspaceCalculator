5dbd09569c703d37b3e191e515ef151d9b4e1f6d
Victor Olowe <vikie95@gmail.com>
2024-09-10 12:15:44 +0100

    fix build

--- airspacecalculator/app/components/MapMarkerPopup.tsx
+++ airspacecalculator/app/components/MapMarkerPopup.tsx
@@ -17,28 +17,20 @@ interface IMapMarkerPopupProps {
 
 function MapMarkerPopup(props: IMapMarkerPopupProps) {
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center mr-1">
-          {getIcon('buildings')}
-          <span className={amountLabelClassNames}>
-            {formatAmount(Number(props.estimatedValue))}
-          </span>
-        </div>
-
-        <div className="flex items-center ml-1">
-          {getIcon('priceCoin')}
-          <span className={amountLabelClassNames}>
-            {formatAmount(Number(props.estimatedAnnualProjection))}
-          </span>
-        </div>
+    <div className="flex items-center justify-between">
+      <div className="flex items-center mr-1">
+        {getIcon('buildings')}
+        <span className={amountLabelClassNames}>
+          {formatAmount(Number(props.estimatedValue))}
+        </span>
       </div>
 
-      {props.isAverageEstimate && (
-        <span className="text-xs text-icon font-campton">
-          Estimated value is based on area statistics.
+      <div className="flex items-center ml-1">
+        {getIcon('priceCoin')}
+        <span className={amountLabelClassNames}>
+          {formatAmount(Number(props.estimatedAnnualProjection))}
         </span>
-      )}
+      </div>
     </div>
   );
 }
